2ce5c4cd8c418ffadb79c6a29943ae6d70940cd9
likeRudin <likeRudin@gmail.com>
2023-09-08 16:11:10 +0900

    add type for Movie

--- src/type.d.ts
+++ src/type.d.ts
@@ -1,2 +1,16 @@
-
-  
+export interface MovieData {
+    id: number;
+    name: string;
+    description: string;
+    modified: string;
+    resourceURI: string;
+    urls: object;
+    comics: string[];
+    stories: string[];
+    events: [];
+    series: [];
+    thumbnail: {
+      path: string,
+      extension: string
+    }
+  }
