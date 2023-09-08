6c5cbb6594715eac25988423d5e0ed1242dda01f
likeRudin <likeRudin@gmail.com>
2023-09-08 16:11:45 +0900

    add uri path to detail page

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,13 +1,18 @@
 import { createBrowserRouter } from "react-router-dom";
 import ErrorPage from "./error-page";
 import Home from "./routes/Home";
+import Detail from "./routes/Detail";
 
 const Router = createBrowserRouter([
     {
         path:'/',
         element:<Home />,
-        errorElement: <ErrorPage />
+        errorElement: <ErrorPage />,
     },
+    {
+        path:'movies/:movieId',
+        element: <Detail />
+    }
 ])
 
 export default Router
