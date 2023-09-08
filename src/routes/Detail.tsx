61172c18df2b1365e6dbcd60923a0de4cf93f591
likeRudin <likeRudin@gmail.com>
2023-09-08 21:07:03 +0900

    seperate Tab and TabContent from jsx

--- src/routes/Detail.tsx
+++ src/routes/Detail.tsx
@@ -1,15 +1,38 @@
 import { useLocation } from "react-router-dom";
+import { MovieData } from "../type";
+import {useState} from "react";
+import Movie from "../components/Movie";
+import Tab from "../components/Tab";
+import TabContent from "../components/TabContent";
+import { TabKind } from "../type";
 
 const Detail = () => {
     const location = useLocation();
-    console.log(location);
-    const {name, thumbnail} = location.state.movie;
+    const { name, thumbnail, comics, series, description } = location.state.movie as MovieData;
+  
+    const [activeTab, setActiveTab] = useState<TabKind>("description");
+  
+    const handleTabChange = (tabName:TabKind) => {
+      setActiveTab(tabName);
+    };
     return (
         <>
-        {`${name}`}
-        {thumbnail? <img src={`${thumbnail.path}.${thumbnail.extension}`}  />: null }
+          <Movie name={name} thumbnail={thumbnail}/>
+          <div>
+            <ul>
+              <Tab tabName="description" activeTab={activeTab} handleTabChange={handleTabChange} />
+              <Tab tabName="comics" activeTab={activeTab} handleTabChange={handleTabChange} />
+              <Tab tabName="series" activeTab={activeTab} handleTabChange={handleTabChange} />
+            </ul>
+            <TabContent
+              activeTab={activeTab}
+              description={description}
+              comics={comics}
+              series={series}
+            />
+          </div>
         </>
-    )
-    
-  }
+      );
+    };
+  
   export default Detail;
