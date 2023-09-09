03ba41a28dd4e7a30cf6354e56bb183aec41fd26
likeRudin <likeRudin@gmail.com>
2023-09-09 18:00:56 +0900

    api modify

--- src/apis/getMovies.ts
+++ src/apis/getMovies.ts
@@ -1,6 +1,6 @@
 export const getMovies = async () => {
     const json = await( await fetch(
-      `https://marvel-proxy.nomadcoders.workers.dev/v1/public/characters?limit=50`
+      `https://marvel-proxy.nomadcoders.workers.dev/v1/public/characters?limit=50&orderBy=modified&series=24229,1058,2023`
     )).json();
     return json.data.results;
 }
